a0999e7b2f11feb47cfdd00ac47e055145f148b1
Mark S. Lewis <mark_lewis@uk.ibm.com>
2020-11-03 17:40:18 +0000

    Update dependencies
    Also:
    - Switch to v8 code coverage provider for accurate TypeScript coverage
    - Build with Ubuntu 20.04
    - Test with Node v14 (in addition to v10 and v12)
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     collectCoverage: true,
+    coverageProvider: "v8",
     testPathIgnorePatterns: [
         "/node_modules/",
         "/scenario/"
